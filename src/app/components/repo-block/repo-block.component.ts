dd4040128bdd606067613200277868d35b2bc0ef
Kate Boyd <kwboyd95@gmail.com>
2018-01-28 19:47:16 -0500

    Adding mobile repo styles.

--- src/app/components/repo-block/repo-block.component.ts
+++ src/app/components/repo-block/repo-block.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, trigger, transition, animate, style } from '@angular/core';
 import { Repo } from '../../interfaces/repo';
+import { RepoColorService } from '../../services/repo-color/repo-color.service';
 
 @Component({
   selector: 'repo-block',
@@ -19,6 +20,19 @@ import { Repo } from '../../interfaces/repo';
 export class RepoBlockComponent {
   @Input() repo: Repo;
 
-  constructor() { }
+  constructor(public repoColorService: RepoColorService) { }
 
+  getColor(language) {
+    let color = this.repoColorService.getColor(language);
+    if (!language) {
+      // if no language, don't want a background
+      return 'transparent';
+    } else if (color) {
+      // return the color associated with the language
+      return color;
+    } else {
+      // if no color associated with language, default to dark blue
+      return '#3176a5';
+    }
+  }
 }
